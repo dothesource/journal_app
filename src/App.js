15aa3888326ced0c77b08ad481b3761cab03c6b3
Danny Williams <danny.h.williams@hotmail.com>
2019-03-02 19:10:15 +0000

    add request for entries

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [entries, setEntries] = useState([])
+  fetch('http://localhost:4000/entries.json', {})
+    .then(response => response.json())
+    .then(entries => setEntries(entries))
+  return <div className="container">{entries.map(entry => entry.text)}</div>
 }
 
-export default App;
+export default App
