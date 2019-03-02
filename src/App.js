bb22c92877873ef98f3cca093b9d1c01d31eecb8
Danny Williams <danny.h.williams@hotmail.com>
2019-03-02 19:16:56 +0000

    fix noob mistake

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
   const [entries, setEntries] = useState([])
-  fetch('http://localhost:4000/entries.json', {})
-    .then(response => response.json())
-    .then(entries => setEntries(entries))
+  useEffect(() => {
+    fetch('http://localhost:4000/entries.json', {})
+      .then(response => response.json())
+      .then(entries => setEntries(entries))
+  }, [])
+
   return <div className="container">{entries.map(entry => entry.text)}</div>
 }
 
