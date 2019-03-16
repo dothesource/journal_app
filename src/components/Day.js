7d57c9d41b0fe4b33680e464f2d12f9f435c51e6
Danny Williams <danny.h.williams@hotmail.com>
2019-03-16 19:29:50 +0000

    day

---
+++ src/components/Day.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Entry from './Entry'
+const Day = ({ day, deleteEntry }) => {
+  return (
+    <React.Fragment>
+      {day &&
+        day.entries &&
+        day.entries.map(entry => (
+          <Entry key={entry.id} entry={entry} deleteEntry={deleteEntry} />
+        ))}
+    </React.Fragment>
+  )
+}
+export default Day
