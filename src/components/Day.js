66748deb028c3117c0d4d6c70fa0bd6452a0d521
Danny Williams <danny.h.williams@hotmail.com>
2019-03-16 19:56:43 +0000

    aqui oh

--- src/components/Day.js
+++ src/components/Day.js
@@ -3,6 +3,7 @@ import Entry from './Entry'
 const Day = ({ day, deleteEntry }) => {
   return (
     <React.Fragment>
+      <h5>{day.title}</h5>
       {day &&
         day.entries &&
         day.entries.map(entry => (
