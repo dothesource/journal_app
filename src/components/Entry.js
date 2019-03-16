1ac52b5839ce8c22ddc90212244c6fe3614b7497
Danny Williams <danny.h.williams@hotmail.com>
2019-03-16 21:27:51 +0000

    its like keep

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import Card, {
-  CardActions,
-  CardActionIcons,
-  CardPrimaryContent
-} from '@material/react-card'
+import React, { useState } from 'react'
+import Card, { CardActions, CardActionIcons } from '@material/react-card'
 import '@material/react-card/dist/card.css'
+import TextareaAutosize from 'react-textarea-autosize'
 
-const Entry = ({ entry, deleteEntry }) => (
-  <Card className="card">
-    <CardPrimaryContent>
-      <p>{entry.text}</p>
-    </CardPrimaryContent>
-    <CardActions>
-      <CardActionIcons>
-        <i onClick={() => deleteEntry(entry)} className="material-icons">
-          delete
-        </i>
-      </CardActionIcons>
-    </CardActions>
-  </Card>
-)
+const Entry = ({ entry, deleteEntry }) => {
+  const [text, setText] = useState(entry.text)
+  return (
+    <Card className="card">
+      <TextareaAutosize
+        useCacheForDOMMeasurements
+        style={{
+          outline: 'none',
+          border: 'none',
+          resize: 'none',
+          fontFamily: 'Roboto',
+          fontSize: '16px'
+        }}
+        onChange={({ target: { value } }) => {
+          setText(value)
+          console.log(value)
+        }}
+        defaultValue={text}
+      />
+      <CardActions>
+        <CardActionIcons>
+          <i onClick={() => deleteEntry(entry)} className="material-icons">
+            delete
+          </i>
+        </CardActionIcons>
+      </CardActions>
+    </Card>
+  )
+}
 
 export default Entry
