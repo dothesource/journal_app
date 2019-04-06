9295374ee41181b565b0b32425587f36609c6fcc
Danny Williams <danny.h.williams@hotmail.com>
2019-04-06 19:42:04 +0100

    last checks for undefined list

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -1,3 +1,14 @@
 export function last(array) {
-  return array.slice(-1).pop()
+  if (array && array.length > 0) {
+    return array.slice(-1).pop()
+  }
+  return
+}
+
+export function matchingLists(listA = [], listB = []) {
+  const required = new Set(listA)
+  for (var elem of listB) {
+    required.delete(elem)
+  }
+  return required.size === 0
 }
